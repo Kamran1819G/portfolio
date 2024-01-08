62f38134e43830a3326d5909229394975ac64540
Kamran Khan <kamrankhan.sde@gmail.com>
2024-01-08 22:45:26 +0530

    Updated

--- src/components/home/project/ProjectCard.jsx
+++ src/components/home/project/ProjectCard.jsx
@@ -3,91 +3,92 @@ import { Card } from "react-bootstrap";
 import "./ProjectCard.scss";
 
 function ProjectCard(props) {
+  const renderTechStackBadges = () => {
+    if (!props.techStack || !Array.isArray(props.techStack)) {
+      return null;
+    }
+
+    return props.techStack.map((tech, index) => (
+      <span
+        key={index}
+        className="badge"
+        style={{ background: getRandomColor() }}
+      >
+        {tech}
+      </span>
+    ));
+  };
+
+  const getRandomColor = () => {
+    const colors = [
+      "#8993be",
+      "#e34c26",
+      "#264de4",
+      "#f0db4f",
+      "#7cc5d9",
+      "#8B7D6B",
+      "#ed272c",
+      "#3f4cb0",
+      "#30b8f6",
+      "#0a5d9e",
+      "#f58613",
+    ];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
   return (
     <>
       <Card style={{ width: "20rem" }} className="project-card">
         <Card.Img variant="top" src={props.img} />
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
-          <div className="tech-stack">
-            <span className="badge" style={{ background: "#8993be" }}>
-              {props.php}
-            </span>
-            <span className="badge" style={{ background: "#e34c26" }}>
-              {props.html}
-            </span>
-            <span className="badge" style={{ background: "#264de4" }}>
-              {props.css}
-            </span>
-            <span className="badge" style={{ background: "#f0db4f" }}>
-              {props.javascript}
-            </span>
-            <span className="badge" style={{ background: "#7cc5d9" }}>
-              {props.react}
-            </span>
-            <span className="badge" style={{ background: "#8B7D6B" }}>
-              {props.python}
-            </span>
-            <span className="badge" style={{ background: "#ed272c" }}>
-              {props.java}
-            </span>
-            <span className="badge" style={{ background: "#3f4cb0" }}>
-              {props.cpp}
-            </span>
-            <span className="badge" style={{ background: "" }}>
-              {props.nextjs}
-            </span>
-            <span className="badge" style={{ background: "" }}>
-              {props.typescript}
-            </span>
-            <span className="badge" style={{ background: "#30b8f6" }}>
-              {props.flutter}
-            </span>
-            <span className="badge" style={{ background: "#0a5d9e" }}>
-              {props.dart}
-            </span>
-            <span className="badge" style={{ background: "#f58613" }}>
-              {props.firebase}
-            </span>
-            <span className="badge" style={{ background: "" }}>
-              {props.mysql}
-            </span>
-            <span className="badge" style={{ background: "" }}>
-              {props.mongodb}
-            </span>
-            <span className="badge" style={{ background: "" }}>
-              {props.nodejs}
-            </span>
-            <span className="badge" style={{ background: "" }}>
-              {props.expressjs}
-            </span>
-            <span className="badge" style={{ background: "" }}>
-              {props.git}
-            </span>
-
-          </div>
+          <div className="tech-stack">{renderTechStackBadges()}</div>
           <Card.Text>{props.description}</Card.Text>
-          <div className="project-card-links">
-            <a
-              href={props.sourcecode}
-              target="_blank"
-              rel="noreferrer"
-              className="btn-source-code btn btn-secondary"
-            >
-              <i className="bi bi-code-slash"></i>
-            </a>
-            <a
-              href={props.liveview}
-              target="_blank"
-              rel="noreferrer"
-              className="btn-live-view btn"
-              style={{
-                background: "var(--primary-color)",
-                color: "var(--text-primary-color)",
-              }}
-            >
-              {props.btnText}
-            </a>
+          <div className="project-card-links mt-4">
+            {props.sourcecode === "" ? null : (
+              <a
+                href={props.sourcecode}
+                target="_blank"
+                rel="noreferrer"
+                className="rounded-3 btn-source-code btn btn-secondary"
+              >
+                {props.liveview === "" ? (
+                  <span>
+                    <i className="bi bi-code-slash"></i> View Project
+                  </span>
+                ) : (
+                  <span>
+                    <i className="bi bi-code-slash"></i>
+                  </span>
+                )}
+              </a>
+            )}
+            {props.liveview === "" ? null : (
+              <a
+                href={props.liveview}
+                target="_blank"
+                rel="noreferrer"
+                className="rounded-3 btn-live-view btn"
+                style={{
+                  background: "var(--primary-color)",
+                  color: "var(--text-primary-color)",
+                }}
+              >
+                Live View
+              </a>
+            )}
+            {props.staytune === false ? null : (
+              <button
+                className="rounded-3 btn-live-view btn"
+                style={{
+                  background: "var(--primary-color)",
+                  color: "var(--text-primary-color)",
+                }}
+              >
+                Stay Tune
+              </button>
+            )}
           </div>
         </Card.Body>
       </Card>
